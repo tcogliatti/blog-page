e889cc9644e10664367c71fcef2b5e881a2588b0
TC <tc@MacBook-Pro-de-TC.local>
2023-08-14 23:16:27 -0300

    new (user-admin): crud of users sistem

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -117,4 +117,27 @@ export class User {
             throw error;
         }
     }
+
+    async deleteUser(tokenAccess, idUser) {
+        try {
+            const url = `${this.baseApi}/${ENV.API_ROUTES.USER}/${idUser}`;
+            const params = {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${tokenAccess}`
+                },
+            };
+
+            const response = await fetch(url, params);
+            const result = await response.json();
+
+            if (response.status !== 200)
+                throw result;
+
+            return result;
+            
+        } catch (error) {
+            throw(error);
+        }
+    }
 }
--- client/src/componentes/Admin/Users/UserItem/UserItem.js
+++ client/src/componentes/Admin/Users/UserItem/UserItem.js
@@ -4,19 +4,62 @@ import { image } from "../../../../assets";
 import { ENV } from "../../../../utils";
 import { BasicModal } from "../../../Shared"
 import { UserForm } from "../UserForm";
+import { User } from "../../../../api";
+import { useAuth } from "../../../../hooks";
 import "./UserItem.scss";
 
+const userController = new User();
+
 export function UserItem(props) {
+    const { accessToken } = useAuth();
+            
     const { user, onReload } = props;
     const [showModal, setShowModal] = useState(false);
     const [titleModal, setTitleModal] = useState("");
 
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [confirmMessage, setConfirmMessage] = useState("");
+    const [isDelete, setIsDelete] = useState(false);
+
     const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
+    const onOpenCloseConfirm = () => setShowConfirm((prevState) => !prevState);
+
+    const onActivateDesactivate = async () => {
+        try {
+            await userController.updateUser(accessToken, user._id, { active: !user.active });
+            onReload();
+            onOpenCloseConfirm();
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const openUpdateUSer = () => {
         setTitleModal(`Modificar ${user.email}`);
         onOpenCloseModal();
-    }
+    };
+
+    const openDesactivateActivateConfirm = () => {
+        setIsDelete(false);
+        setConfirmMessage(user.active ? `Desactivar usuario ${user.email}` : `Activar usuario ${user.email}`);
+        onOpenCloseConfirm();
+    };
+
+    const openDeleteConfirm = () => {
+        setIsDelete(true);
+        setConfirmMessage(`Eliminar usuario ${user.email}`);
+        onOpenCloseConfirm();
+    };
+
+    const onDelete = async () => {
+        try {
+            await userController.deleteUser(accessToken, user._id);
+            onReload();
+            onOpenCloseConfirm();
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <>
@@ -32,10 +75,10 @@ export function UserItem(props) {
                     <Button icon primary onClick={openUpdateUSer}>
                         <Icon name='pencil' />
                     </Button>
-                    <Button icon color={user.active ? "orange" : "teal"}>
+                    <Button icon color={user.active ? "orange" : "teal"} onClick={openDesactivateActivateConfirm}>
                         <Icon name={user.active ? "ban" : "check"} />
                     </Button>
-                    <Button icon color='red'>
+                    <Button icon color='red' onClick={openDeleteConfirm}>
                         <Icon name='trash' />
                     </Button>
                 </div>
@@ -44,6 +87,14 @@ export function UserItem(props) {
             <BasicModal show={showModal} close={onOpenCloseModal} title={titleModal}>
                 <UserForm close={onOpenCloseModal} onReload={onReload} user={user} />
             </BasicModal>
+
+            <Confirm
+                open={showConfirm}
+                onCancel={onOpenCloseConfirm}
+                onConfirm={isDelete ? onDelete : onActivateDesactivate}
+                content={confirmMessage}
+                size="mini"
+            />
         </>
     )
 }
